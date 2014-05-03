7fc1a439aad1affbd1a765bbe4793765e5b7ba8c
denny2trasy <denny2trasy@gmail.com>
2014-05-03 23:32:13 +0800

    assets for production

--- config/application.rb
+++ config/application.rb
@@ -27,5 +27,7 @@ module Oenglish
     # The default locale is :en and all translations from config/locales/*.rb,yml are auto loaded.
     # config.i18n.load_path += Dir[Rails.root.join('my', 'locales', '*.{rb,yml}').to_s]
     config.i18n.default_locale = "zh-CN"
+    
+    config.assets.precompile += ['admin.js', 'admin.css', 'student.js','student.css', 'application.js','application.css']
   end
 end
